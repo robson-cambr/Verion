d131f3ae21896b69cf819825c6a3f9f48cf658fd
Robson Ambrosio <robson.cambr@gmail.com>
2023-07-17 20:01:48 -0300

    Register and Login Pages RAW

--- Areas/Identity/Data/VerionCommonUser.cs
+++ Areas/Identity/Data/VerionCommonUser.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Verion.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the VerionCommonUser class
 public class VerionCommonUser : IdentityUser
 {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(256)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(256)")]
+        public string LastName { get; set; }
 }
 
