dca74e0f8b7607c4535976527fc6203dec1fed7b
Robson Ambrosio <robson.cambr@gmail.com>
2023-07-19 20:35:51 -0300

    SideMenu Integration

---
+++ Areas/VerionAnalysis/Pages/SideMenu/Teste.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Verion.Areas.VerionApplication.Pages.SideMenu
+{
+    public class TesteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Areas/VerionAnalysis/Pages/_AnalysisLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Verion.Areas.VerionApplication.Pages
+{
+    public class _VerionApplicationLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
